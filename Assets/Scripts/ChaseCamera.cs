cd933249363b06283baaba79c43375a9ff471081
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2019-11-12 16:11:13 -0500

    Small level tweaks
    Also the chase camera now knows when it is hitting walls and tries to bring itself closer to the player until it isnt. When it leaves a wall it slowly pans back out to normal distance until it reaches desiredDistance or collides again and retracts.

--- Assets/Scripts/ChaseCamera.cs
+++ Assets/Scripts/ChaseCamera.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class ChaseCamera : MonoBehaviour
 {
     public Transform car;
-    public float distance;
-    public float height;
     public float rotationDamping = 3f;
     public float heightDamping = 2f;
     private float desiredAngle = 0;
+    private float distance = 8;
+    private float desiredDistance;
+    private float height = 3.75f;
+    private bool cameraReset = false;
+
+    private void Start()
+    {
+        desiredDistance = distance;
+    }
 
     private void FixedUpdate()
     {
@@ -42,23 +49,28 @@ public class ChaseCamera : MonoBehaviour
         transform.LookAt(car);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        distance -= 0.5f;
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        distance += 0.5f;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        distance -= 0.5f;
+        distance -= 2f;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        distance += 0.5f;
+        CameraSlowReset();
+    }
+    private void Update()
+    {
+        if (distance <= desiredDistance && cameraReset)
+        {
+            distance += 0.01f;
+            return;
+        }
+        cameraReset = false;
+    }
+
+    private void CameraSlowReset()
+    {
+        cameraReset = true;
     }
 }
 
